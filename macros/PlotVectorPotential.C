58c1b24f244bc373886e3d70bbda7cc29a1a8046
Alberto Martinez de la Ossa <delaossa@mail.desy.de>
2016-06-03 01:51:57 +0200

    ...

--- macros/PlotVectorPotential.C
+++ macros/PlotVectorPotential.C
@@ -13,25 +13,12 @@
 #include <TCanvas.h>
 #include <TMath.h>
 
+
 #include "PData.hh"
 #include "PDataHiP.hh"
 #include "PGlobals.hh"
 #include "PPalette.hh"
 
-
-Double_t *Derivative(Double_t a[], Int_t n)
-{
-    Double_t firstDer(0.0);
-    Double_t t(0.001);
-    Double_t *dx = new Double_t[n];
-    
-    for (Int_t i=0;i<n;i++) {
-      if(i<2 || i>=n-2)  dx[i] = 0.0;
-      dx[i] = (   4.0 / 3.0 * (a[i + 1] - a[i - 1]) / (2.0 * t)
-		  - 1.0 / 3.0 * (a[i + 2] - a[i - 2]) / (4.0 * t) );
-    }
-    return dx;
-}
 
 void PlotVectorPotential( const TString &sim, Int_t timestep, const TString &options="") {
   
@@ -65,125 +52,176 @@ void PlotVectorPotential( const TString &sim, Int_t timestep, const TString &opt
   // Time in OU
   Double_t Time = pData->GetRealTime();
 
+
+  cout << Form(" Getting histos..." );
+
+  // Electron density (plasma)
   char hName[36];
   sprintf(hName,"hDen2D_0"); 
   TH2F *hDen2D = (TH2F*) ifile->Get(hName);
-
+  
+  // Electron density species 2 (if any)
   sprintf(hName,"hDen2D_1"); 
   TH2F *hDen2D_1 = (TH2F*) ifile->Get(hName);
   if(hDen2D_1)
     hDen2D->Add(hDen2D_1,1);
 
+  // Electron density species 3 (if any)
   sprintf(hName,"hDen2D_2"); 
   TH2F *hDen2D_2 = (TH2F*) ifile->Get(hName);
   if(hDen2D_2)
     hDen2D->Add(hDen2D_2,1);
 
-  sprintf(hName,"hE2D_1"); 
-  TH2F *hEx2D = (TH2F*) ifile->Get(hName);
-  TH2F *hEx2Dsta = (TH2F*) hEx2D->Clone("hEx2Dsta");
-  hEx2Dsta->Reset();
   
+  cout << Form(" done!" ) << endl;
+
   // Get the sliced 1D histograms 
   Int_t NBinsZ = hDen2D->GetXaxis()->GetNbins();
+  Float_t zmin = hDen2D->GetXaxis()->GetBinLowEdge(1);
+  Float_t zmax = hDen2D->GetXaxis()->GetBinUpEdge(NBinsZ);
+  Float_t zrange = zmax-zmin;
+
   Int_t NBinsX = hDen2D->GetYaxis()->GetNbins();
-  Double_t xmin = hDen2D->GetYaxis()->GetBinLowEdge(1);
-  Double_t xmax = hDen2D->GetYaxis()->GetBinUpEdge(NBinsX);
-  Double_t xrange = xmax-xmin;
+  Float_t xmin = hDen2D->GetYaxis()->GetBinLowEdge(1);
+  Float_t xmax = hDen2D->GetYaxis()->GetBinUpEdge(NBinsX);
+  Float_t xrange = xmax-xmin;
 
+  // cout << Form(" Creating 2D histos..." );
+  // cout << Form(" done!" ) << endl;
 
-  TH1D **hDen1D = new TH1D*[NBinsZ]; 
-  TH1  **hDen1Dt = new TH1*[NBinsZ];
-  TH1  **hPhi1D  = new TH1*[NBinsZ];
-  TH1  **hEx1D   = new TH1*[NBinsZ];
-  TH1  **hDen1Db = new TH1*[NBinsZ];
-  TH1D *hDen1Dion = 0;
+  cout << Form(" Allocating array with real data..." );
 
-  Double_t kmin, kmax;
+  Int_t dims[2] = {NBinsZ,NBinsX};
+  Double_t *data = new Double_t[NBinsZ*NBinsX];
   
-  //  TVirtualFFT::SetTransform(0);
-  for(Int_t i=NBinsZ; i>0; i--) {
-    TVirtualFFT::SetTransform(0);
-    // cout << Form(" Bin = %i",i) << endl;
+  // TVirtualFFT::SetTransform(0);
+  
+  cout << Form(" done!" ) << endl;
 
-    // Get 1D histograms for each longitudinal slice
-    hDen1D[i-1] = hDen2D->ProjectionY("",i,i); 
-    sprintf(hName,"hDen1D_x_%i",i); 
-    hDen1D[i-1]->SetName(hName);
-
-    // Get ion distribution
-    if(i==NBinsZ) {
-      hDen1Dion = (TH1D*) hDen1D[i-1]->Clone("hDen1Dion");      
-    }
-
-    // and substract it to the electron density 
-    hDen1D[i-1]->Add(hDen1Dion,-1);
+  cout << Form(" Filling data aray..." );
+  for(Int_t i=0; i<NBinsZ; i++) {
     
-    // Get Transform
-    hDen1Dt[i-1] = 0;
-    hDen1Dt[i-1] = hDen1D[i-1]->FFT(hDen1Dt[i-1],"Re");
-    sprintf(hName,"hDen1Dt_x_%i",i); 
-    hDen1Dt[i-1]->SetName(hName);
+    for(Int_t j=0; j<NBinsX; j++) {
 
-    TVirtualFFT *fft = TVirtualFFT::GetCurrentTransform();
-    Int_t n = NBinsX;
-
-    // Set the frequency range
-    if(i==NBinsZ) {
-      kmin = 0.0;
-      kmax = TMath::TwoPi() * hDen1Dt[i-1]->GetNbinsX()/xrange;
-    }    
-    hDen1Dt[i-1]->SetBins(NBinsX,kmin,kmax);
-    hDen1Dt[i-1]->Scale(TMath::Sqrt(1.0/NBinsX));
-
-    Double_t *re_full = new Double_t[n];
-    Double_t *im_full = new Double_t[n];
-    fft->GetPointsComplex(re_full,im_full);
-
-    
-    for(Int_t j=0;j<n;j++) {
-      //Double_t k = (j*(n-1.0)/n - (n/2.0)) * (TMath::TwoPi()/xrange);
-      //Double_t k = (j*(n-1.0)/n - (n/2.0)) * (TMath::Pi()/xrange);
-      Double_t k = hDen1Dt[i-1]->GetBinCenter(j+1);
-      //Double_t k = ((j+1)/xrange);
-      Double_t k2 = k * k;
-      // if(i==NBinsZ)
-      //  	cout << Form(" k = %.4e  k2 = %.4e",k,k2) << endl;
-      re_full[j] /= k2;
-      im_full[j] /= k2;
-    }
-    
-    //Now let's make a backward transform:
-    TVirtualFFT *fft_back = TVirtualFFT::FFT(1, &n, "C2R EX K");
-    fft_back->SetPointsComplex(re_full,im_full);
-    fft_back->Transform();
-
-    hPhi1D[i-1] = 0;
-    hPhi1D[i-1] = TH1::TransformHisto(fft_back,hPhi1D[i-1],"RE");
-    sprintf(hName,"hPhi1D_x_%i",i); 
-    hPhi1D[i-1]->SetName(hName);
-    hPhi1D[i-1]->SetBins(NBinsX,xmin,xmax);
-    hPhi1D[i-1]->Scale(1.0/NBinsX);    
-
-    // Derivative
-    sprintf(hName,"hEx1D_x_%i",i); 
-    hEx1D[i-1] = new TH1D(hName,"",NBinsX,xmin,xmax);
-
-    Double_t dt = hPhi1D[i-1]->GetBinWidth(1);
-    for(Int_t j=0;j<hPhi1D[i-1]->GetNbinsX();j++) {
-      Double_t der = 0.;
-      if(j>2 && j<=n-2) {
-	
-	der =  (   4.0 / 3.0 * (hPhi1D[i-1]->GetBinContent(j+1) - hPhi1D[i-1]->GetBinContent(j-1))/ (2.0 * dt)
-		   
-		   - 1.0 / 3.0 * (hPhi1D[i-1]->GetBinContent(j+2) - hPhi1D[i-1]->GetBinContent(j-2)) / (4.0 * dt) );
-	
-      }
+      Int_t index =  i * NBinsX + j;
       
-      hEx1D[i-1]->SetBinContent(j+1,der);
-      hEx2Dsta->SetBinContent(i,j+1,der);
+      data[index]  = hDen2D->GetBinContent(i+1,j+1);
+
+      // substract ion background
+      data[index] -= 1;
+    }
+  }
+  
+  cout << Form("   done!" ) << endl;
+  
+  cout << Form(" Fourier transform ..." );
+
+  // TH2F *hFFTREb = new TH2F("hFFTREb","",NBinsZ,kzmin,kzmax,NBinsX,kxmin,kxmax);
+  // TH2F *hFFTIMb = new TH2F("hFFTIMb","",NBinsZ,kzmin,kzmax,NBinsX,kxmin,kxmax);
+  // TH2F *hFFTREb = new TH2F("hFFTREb","",NBinsZ,0.,NBinsZ,NBinsX,0.,NBinsX);
+  // TH2F *hFFTIMb = new TH2F("hFFTIMb","",NBinsZ,0.,NBinsZ,NBinsX,0.,NBinsX);
+  
+  // TH2F *hFFTRE = 0;
+  // hFFTRE = (TH2F*) TH1::TransformHisto(fft, hFFTRE, "RE");
+  // hFFTRE->SetName("hFFTRE");
+  // TH2F *hFFTIM = 0;
+  // hFFTIM = (TH2F*) TH1::TransformHisto(fft, hFFTIM, "IM");
+  // hFFTIM->SetName("hFFTIM");
+
+  TVirtualFFT *fft = TVirtualFFT::FFT(2, dims, "R2C ES K");
+  fft->SetPoints(data);
+  fft->Transform();  
+  cout << Form("   done!" ) << endl;
+  
+   cout << Form(" Solving equation for potential in fourier space" )  << endl;
+  // \phi(kz,kx) = 1/(kz^2 + kx^2) 
+  
+  fft->GetPoints(data);    
+  for(Int_t i=0; i<dims[0]; i++) {
+    for(Int_t j=0; j<(dims[1]/2+1); j++) {
+
+      Int_t index =  2 * (i * (dims[1]/2+1)  + j);
+      
+      Float_t kz;
+      if(i<dims[0]/2)
+	kz = TMath::Pi() * (i+0.5) / zrange;
+      else
+	kz = -TMath::Pi() * (dims[0]-i+0.5) / zrange;     
+      Float_t kx = TMath::TwoPi() * (j+0.5) / xrange;
+      
+      Float_t k2 = kx*kx + kz*kz;
+      
+      data[index] /= k2;
+      data[index+1] /= k2;
+      
     }
     
   }
+  cout << Form("   done!" ) << endl;
   
+  cout << Form(" Inverse Fourier transform ..." );
+  // backward transform:
+  TVirtualFFT *fft_back = TVirtualFFT::FFT(2, dims, "C2R ES K");
+  fft_back->SetPoints(data);
+  fft_back->Transform();
+  
+  cout << Form(" done!" ) << endl;
+
+  Double_t *re_back = fft_back->GetPointsReal();
+  
+  TH2F *hPhi2D = new TH2F("hPhi2D","",NBinsZ,zmin,zmax,NBinsX,xmin,xmax);
+  TH2F *hE2D_1_ifft = new TH2F("hE2D_1_ifft","",NBinsZ,zmin,zmax,NBinsX,xmin,xmax);
+  Double_t dx = hPhi2D->GetYaxis()->GetBinWidth(1);
+  for(Int_t i=0; i<NBinsZ; i++) {
+    for(Int_t j=0; j<NBinsX; j++) {
+      Int_t index =  i * NBinsX + j;
+      
+      hPhi2D->SetBinContent(i+1,j+1,re_back[index]);
+      
+      Double_t der = 0.;
+      if(j>2 && j<=NBinsX-2) {
+	Int_t indjp1 =  i * NBinsX + (j+1);
+	Int_t indjp2 =  i * NBinsX + (j+2);
+	Int_t indjm1 =  i * NBinsX + (j-1);
+	Int_t indjm2 =  i * NBinsX + (j-2);
+	
+	der =  ( 4.0 / 3.0 * (re_back[indjp1] - re_back[indjm1]) / (2.0 * dx)
+	       - 1.0 / 3.0 * (re_back[indjp2] - re_back[indjm2]) / (4.0 * dx) );
+	
+      }
+      
+      hE2D_1_ifft->SetBinContent(i+1,j+1,der);
+    }
+  }
+  hPhi2D->Scale(1.0/(NBinsZ*NBinsX));    
+  hE2D_1_ifft->Scale(1.0/(NBinsZ*NBinsX));
+
+
+  TH2F *hE2D_0_ifft = new TH2F("hE2D_0_ifft","",NBinsZ,zmin,zmax,NBinsX,xmin,xmax);
+  Double_t dz = hPhi2D->GetXaxis()->GetBinWidth(1);
+  for(Int_t j=0; j<NBinsX; j++) {
+    for(Int_t i=0; i<NBinsZ; i++) {
+      Double_t der = 0.;
+      if(i>2 && j<=NBinsZ-2) {
+	Int_t indip1 =  (i+1) * NBinsX + j;
+	Int_t indip2 =  (i+2) * NBinsX + j;
+	Int_t indim1 =  (i-1) * NBinsX + j;
+	Int_t indim2 =  (i-2) * NBinsX + j;
+	
+	der =  ( 4.0 / 3.0 * (re_back[indip1] - re_back[indim1]) / (2.0 * dz)
+		 - 1.0 / 3.0 * (re_back[indip2] - re_back[indim2]) / (4.0 * dz) );
+	
+      }
+      
+      hE2D_0_ifft->SetBinContent(i+1,j+1,der);
+    }
+  }
+  hE2D_0_ifft->Scale(1.0/(NBinsZ*NBinsX));
+  
+  
+  // TH2F *hPhi2D = 0;
+  // hPhi2D = (TH2F*) TH1::TransformHisto(fft_back, hPhi2D, "RE");
+  // hPhi2D->SetName("hPhi2D");
+    
+    
 }
