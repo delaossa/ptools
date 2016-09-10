cfcde835288d81198d5a9f826a4a631ceb5261a4
Alberto Martinez de la Ossa <delaossa@mail.desy.de>
2016-09-10 20:17:55 +0200

    ptools meets python

--- pymacros/PlotJoy.py
+++ pymacros/PlotJoy.py
@@ -1,63 +1,89 @@
 #!/usr/bin/env python
 
 from ROOT import PData, PGlobals, gStyle, TObject, TFile, TCanvas, TH1F
+import sys, argparse
 
-pData = PData('rake-v10kA.G.SR2.RI.3D')
+def main():
 
-pData.LoadFileNames(20)
+    parser = argparse.ArgumentParser(description='Makes a Joy Division style plot.')
+    parser.add_argument('sim', help='simulation name')
+    parser.add_argument('-b', action='store_true', help='run in batch mode without graphics')
+    parser.add_argument('-t', type=int, help='timestep')
+    parser.add_argument('-n', type=int, dest="nlines", default=50, help='number of lines')
+    parser.add_argument('-max', type=float, dest="max", default=100, help='maximum density')
+    parser.add_argument('-f', type=float, dest="factor", default=100, help='vertical factor')
 
-PGlobals.Initialize()
+    try:
+        args = parser.parse_args()
+    except:
+        parser.print_help()
+        sys.exit(0)
+        
+    pData = PData(args.sim)
 
-gStyle.SetCanvasColor(1)
-gStyle.SetFrameFillColor(1)
-gStyle.SetFrameFillColor(1)
-gStyle.SetHistLineColor(0)
-gStyle.SetHistLineWidth(1)
-gStyle.SetPadLeftMargin(0.20)
-  
-ofile = TFile("joy.root","RECREATE")
-NbinsX1 = pData.GetX1N()
+    pData.LoadFileNames(args.t)
+    pData.PrintData()
 
-hDen1D = []
-#TH1F **hDen1D = new TH1F*[NbinsX1]
+    PGlobals.Initialize()
 
-TOP = 100
-shift = 0.2
-MAX = -999
-MAX2 = 2*(NbinsX1 * shift) 
-  
-C = TCanvas("C","",800,1280)
-  
-j =  (pData.GetX3iMin() + pData.GetX3iMax()) / 2
+    gStyle.SetCanvasColor(1)
+    gStyle.SetFrameFillColor(1)
+    gStyle.SetHistLineColor(0)
+    gStyle.SetHistLineWidth(2)
 
-for i in range(pData.GetX1iMin(),pData.GetX1iMax()) :
+    gStyle.SetPadLeftMargin(0.20)
+    gStyle.SetPadRightMargin(0.20)
+    gStyle.SetPadTopMargin(0.20)
+    gStyle.SetPadBottomMargin(0.20)
     
-    hName = "hDen1D_%i" % i
-    if pData.Is3D() :
-        hDen1D.append(pData.GetH1SliceX3D(pData.GetChargeFileName(0).c_str(),"charge",i,i,j,j,"avg"))
+    ofile = TFile("joy.root","RECREATE")
+    NbinsX1 = pData.GetX1N()
 
-    hDen1D[i].SetName(hName)
+    hDen1D = []
 
-    for k in range (1,hDen1D[i].GetNbinsX()+1) :
-
-        if(hDen1D[i].GetBinContent(k)<TOP) :
-	    hDen1D[i].SetBinContent(k,hDen1D[i].GetBinContent(k) + i*shift)
-        else :
-	    hDen1D[i].SetBinContent(k,TOP + i*shift)
-
-        if(MAX<hDen1D[i].GetMaximum()) :
-            MAX = hDen1D[i].GetMaximum()
+    N = args.nlines
+    top = args.max
+    shift = 0.2
+    factor = args.factor
+    max = -999
+    max2 = factor * (NbinsX1 * shift) 
     
-#        hDen1D[i].Write(hName,TObject.kOverwrite)
-    
-  
-hFrame = hDen1D[0].Clone("hFrame")
-hFrame.Reset()
-hFrame.GetYaxis().SetRangeUser(0.0,MAX2)
+    C = TCanvas("C","",800,1000)
 
-hFrame.Draw("AC")
-for i in range(pData.GetX1iMax()-1,pData.GetX1iMin(), -NbinsX1/50):
-    hDen1D[i].SetFillColor(1)
-    hDen1D[i].Draw("FC same")
-  
-PGlobals.imgconv(C,"joy","pdf")
+    j =  (pData.GetX3iMin() + pData.GetX3iMax()) / 2
+
+    for i in range(pData.GetX1iMin(),pData.GetX1iMax()) :
+        
+        hName = "hDen1D_%i" % i
+        if pData.Is3D() :
+            hDen1D.append(pData.GetH1SliceX3D(pData.GetChargeFileName(0).c_str(),"charge",i,i,j,j,"avg"))
+
+        hDen1D[i].SetName(hName)
+
+        for k in range (1,hDen1D[i].GetNbinsX()+1) :
+            
+            if(hDen1D[i].GetBinContent(k)<top) :
+	        hDen1D[i].SetBinContent(k,hDen1D[i].GetBinContent(k) + i*shift)
+            else :
+	        hDen1D[i].SetBinContent(k,top + i*shift)
+                
+        if(max<hDen1D[i].GetMaximum()) :
+            max = hDen1D[i].GetMaximum()
+                
+        #        hDen1D[i].Write(hName,TObject.kOverwrite)
+                                
+    hFrame = hDen1D[0].Clone("hFrame")
+    hFrame.Reset()
+    # hFrame.GetYaxis().SetRangeUser(0.0,max2)
+    hFrame.GetYaxis().SetRangeUser(0.0,1.05 * max)
+                    
+    hFrame.Draw("A")
+    for i in range(pData.GetX1iMax()-1,pData.GetX1iMin(), -NbinsX1/N):
+        hDen1D[i].SetFillColor(1)
+        hDen1D[i].Draw("FC same")
+                        
+    PGlobals.imgconv(C,"joy","pdf")
+
+
+if __name__ == '__main__':
+    main()
