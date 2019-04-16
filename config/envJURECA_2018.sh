c879c1dbe3e157235ffd205c80c9d40823a86701
Alberto Martinez de la Ossa <martinezdelaossa1@jrc1384.jureca>
2019-04-16 14:24:21 +0200

    config files for Jureca

--- config/envJURECA_2018.sh
+++ config/envJURECA_2018.sh
@@ -1,15 +1,15 @@
 #!/bin/bash
 
-SOFT=/p/project/chhh45/hhh451/software-jureca
+SOFT=/homea/hhh45/hhh451/software-jureca
 
 # Setting up Root environment....
-export ROOTSYS=$SOFT/root-6.06.08
+export ROOTSYS=$SOFT/root-6.14.04
 export LD_LIBRARY_PATH=$ROOTSYS/lib:$LD_LIBRARY_PATH
 export PATH=$ROOTSYS/bin:$PATH
 export PYTHONPATH=$ROOTSYS/lib:$PYTHONPATH
 
 # HDF5: C++ interface
-export HDF5CPP=/usr/local/software/jureca/Stages/2016a/software/HDF5/1.8.16-iimpi-8.2.5-GCC-4.9.3-2.25
+export HDF5CPP=/usr/local/software/jureca/Stages/2018a/software/HDF5/1.8.20-iimpi-2018a
 export LD_LIBRARY_PATH=$HDF5CPP/lib:$LD_LIBRARY_PATH
 
 # ptools environment:
