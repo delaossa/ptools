549d4cdc616f9a3674e246e7bd47ef4cbc433db4
delaossa <alberto.martinez.de.la.ossa@desy.de>
2019-06-17 21:37:01 +0200

    ..

--- config/envMaconda.sh
+++ config/envMaconda.sh
@@ -1,18 +1,15 @@
 #!/bin/bash
 
-# Adding Anaconda environment (ROOT, HDF5, SZIP) should be installed there....
+# Adding Anaconda environment (ROOT and HDF5) should be installed there....
+export CONDA_BUILD_SYSROOT=/Users/delaossa/local/MacOSX10.9.sdk
 export ROOTSYS=$CONDA_PREFIX
-export LD_LIBRARY_PATH=$ROOTSYS/lib:$LD_LIBRARY_PATH
-export PATH=$ROOTSYS/bin:$PATH
-export PYTHONPATH=$ROOTSYS/lib:$PYTHONPATH
 
 # HDF5: C++ interface
 export HDF5CPP=$CONDA_PREFIX
-export LD_LIBRARY_PATH=$HDF5CPP/lib:$LD_LIBRARY_PATH
 
 # ptools environment:
-export PTOOLS=/Users/delaossa/plasma/software/ptools
-export LD_LIBRARY_PATH=$PTOOLS/lib:$LD_LIBRARY_PATH
+export PTOOLS=/Users/delaossa/local/ptools
+export DYLD_LIBRARY_PATH=$PTOOLS/lib:$DYLD_LIBRARY_PATH
 export PATH=$PTOOLS/bin:$PTOOLS/scripts:$PTOOLS/pymacros:$PATH
-export PYTHONPATH=$PTOOLS/pymacros:$PYTHONPATH
+export PYTHONPATH=$PTOOLS/lib:$PYTHONPATH
 
