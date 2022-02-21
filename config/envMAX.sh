b3de07f907c490340ecf45612fc2638e96e3d5c5
delaossa <alberto.martinez.de.la.ossa@desy.de>
2022-02-21 14:39:03 +0100

    adding alias to setup root env

--- config/envMAX.sh
+++ config/envMAX.sh
@@ -5,6 +5,8 @@ export SOFT=/data/netapp/fla/plasma/software-max
 
 # Setting up Root environment....
 export ROOTSYS=$SOFT/root-6.06.08
+# ROOT setup
+alias thisroot='source $ROOTSYS/bin/thisroot.sh'
 export LD_LIBRARY_PATH=$ROOTSYS/lib:$LD_LIBRARY_PATH
 export PATH=$ROOTSYS/bin:$PATH
 
